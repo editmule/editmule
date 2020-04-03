e5e77bd13b4aa00220a79db44869528f54e7c51a
zanemountcastle <zane.mountcastle@gmail.com>
2020-04-02 18:23:33 -0700

    Made home page time to load faster

--- src/modules/app/App.tsx
+++ src/modules/app/App.tsx
@@ -39,181 +39,170 @@ function App(props: any) {
   }
 
   return (
-    <>
-      {
-        isAuthenticating ?
-          <section className="height-30 text-center">
-            <div className="spinner-border" role="status" >
-              <span className="sr-only">Loading...</span>
-            </div >
-          </section >
-          :
-          <div>
-            <div className="nav-container">
-              <div className="via-1585181384433">
-                <div className="bar bar--sm visible-xs">
-                  <div className="container">
-                    <div className="row">
-                      <div className="col-3 col-md-2">
-                        <LinkContainer to="/">
-                          <div className="hover">
-                            <img className="logo logo-dark" alt="logo" src="/img/logo-dark.png"></img>
-                            <img className="logo logo-light" alt="logo" src="/img/logo-light.png"></img>
-                          </div>
-                        </LinkContainer>
-                      </div>
-                      <div className="col-9 col-md-10 text-right hover">
-                        <div className="hamburger-toggle" data-toggle-class="#menu1;hidden-xs hidden-sm">
-                          <i className="icon icon--sm stack-interface stack-menu"></i>
-                        </div>
-                      </div>
+    <div>
+      <div className="nav-container">
+        <div className="via-1585181384433">
+          <div className="bar bar--sm visible-xs">
+            <div className="container">
+              <div className="row">
+                <div className="col-3 col-md-2">
+                  <LinkContainer to="/">
+                    <div className="hover">
+                      <img className="logo logo-dark" alt="logo" src="/img/logo-dark.png"></img>
+                      <img className="logo logo-light" alt="logo" src="/img/logo-light.png"></img>
                     </div>
+                  </LinkContainer>
+                </div>
+                <div className="col-9 col-md-10 text-right hover">
+                  <div className="hamburger-toggle" data-toggle-class="#menu1;hidden-xs hidden-sm">
+                    <i className="icon icon--sm stack-interface stack-menu"></i>
                   </div>
                 </div>
-                <nav id="menu1" className="bar bar-1 hidden-xs">
-                  <div className="container">
-                    <div className="row">
-                      <div className="col-lg-2 col-md-2 hidden-xs">
-                        <div className="bar__module">
-                          <LinkContainer to="/">
-                            <div className="hover">
-                              <img className="logo logo-dark" alt="logo" src="/img/logo-dark.png"></img>
-                              <img className="logo logo-light" alt="logo" src="/img/logo-light.png"></img>
-                            </div>
-                          </LinkContainer>
-                        </div>
-                      </div>
-                      <div className="col-lg-10 col-md-10 text-right text-left-xs text-left-sm">
-                        {isAuthenticated
-                          ? <>
-                            <div className="bar__module">
-                              <ul className="menu-horizontal text-left">
-                                <li>
-                                  <LinkContainer to="/cart">
-                                    <a href="#/">Cart</a>
-                                  </LinkContainer>
-                                </li>
-                                <li className="dropdown">
-                                  <span className="dropdown__trigger">
-                                    Account
-                            </span>
-                                  <div className="dropdown__container">
-                                    <div className="container">
-                                      <div className="row">
-                                        <div className="dropdown__content col-lg-2">
-                                          <ul className="menu-vertical">
-                                            <li>
-                                              <LinkContainer to="/account/orders">
-                                                <a href="#/">Orders</a>
-                                              </LinkContainer>
-                                            </li>
-                                            <li>
-                                              <LinkContainer to="/account/settings">
-                                                <a href="#/">Settings</a>
-                                              </LinkContainer>
-                                            </li>
-                                            <li className="separate" onClick={handleLogout}>
-                                              <a href="#/">Log Out</a>
-                                            </li>
-                                          </ul>
-                                        </div>
-                                      </div>
-                                    </div>
-                                  </div>
-                                </li>
-                              </ul>
-                            </div>
-                            <div className="bar__module">
-                              <LinkContainer to="/order">
-                                <div className="btn btn--sm btn--primary type--uppercase">
-                                  <div className="btn__text">
-                                    Order Now
-                                  </div>
-                                </div>
-                              </LinkContainer>
-                            </div>
-                          </>
-                          : <>
-                            <div className="bar__module">
-                              <ul className="menu-horizontal text-left">
-                                <li>
-                                  <LinkContainer to="/cart">
-                                    <a href="#/">Cart</a>
-                                  </LinkContainer>
-                                </li>
-                                <li>
-                                  <LinkContainer to="/login">
-                                    <a href="#/">Log In</a>
-                                  </LinkContainer>
-                                </li>
-                              </ul>
-                            </div>
-                            <div className="bar__module">
-                              <LinkContainer to="/signup">
-                                <div className="btn btn--sm type--uppercase">
-                                  <div className="btn__text">Sign Up</div>
-                                </div>
-                              </LinkContainer>
-                              <LinkContainer to="/order">
-                                <div className="btn btn--sm btn--primary type--uppercase">
-                                  <div className="btn__text">Order Now</div>
-                                </div>
-                              </LinkContainer>
-                            </div>
-                          </>
-                        }
-                      </div>
-                    </div>
-                  </div>
-                </nav>
               </div>
             </div>
-            <Routes appProps={{ isAuthenticated, userHasAuthenticated }} />
-            <footer className="footer-3 text-center-xs space--xs ">
-              <div className="container">
-                <div className="row">
-                  <div className="col-md-6">
-                    <img alt="logo" className="logo" src="/img/logo-dark.png" />
-                    <ul className="list-inline list--hover">
-                      <li className="list-inline-item">
-                        <LinkContainer to="/order">
-                          <div className="hover">
-                            <span className="type--fine-print">Get Started</span>
-                          </div>
-                        </LinkContainer>
-                      </li>
-                      <li className="list-inline-item">
-                        <span className="type--fine-print">hello@editmule.com</span>
-                      </li>
-                    </ul>
-                  </div>
-                  <div className="col-md-6 text-md-right text-center-xs">
-                    <ul className="list-inline list--hover">
-                      <li className="list-inline-item">
-                        <span className="type--fine-print">&copy; 2020 Edit Mule</span>
-                      </li>
-                      <li className="list-inline-item">
-                        <LinkContainer to="/privacy">
-                          <div className="hover">
-                            <span className="type--fine-print">Privacy</span>
-                          </div>
-                        </LinkContainer>
-                      </li>
-                      <li className="list-inline-item">
-                        <LinkContainer to="/terms">
-                          <div className="hover">
-                            <span className="type--fine-print">Terms</span>
-                          </div>
-                        </LinkContainer>
-                      </li>
-                    </ul>
+          </div>
+          <nav id="menu1" className="bar bar-1 hidden-xs">
+            <div className="container">
+              <div className="row">
+                <div className="col-lg-2 col-md-2 hidden-xs">
+                  <div className="bar__module">
+                    <LinkContainer to="/">
+                      <div className="hover">
+                        <img className="logo logo-dark" alt="logo" src="/img/logo-dark.png"></img>
+                        <img className="logo logo-light" alt="logo" src="/img/logo-light.png"></img>
+                      </div>
+                    </LinkContainer>
                   </div>
                 </div>
+                <div className="col-lg-10 col-md-10 text-right text-left-xs text-left-sm">
+                  {isAuthenticated && !isAuthenticating
+                    ? <>
+                      <div className="bar__module">
+                        <ul className="menu-horizontal text-left">
+                          <li>
+                            <LinkContainer to="/cart">
+                              <a href="#/">Cart</a>
+                            </LinkContainer>
+                          </li>
+                          <li className="dropdown">
+                            <span className="dropdown__trigger">
+                              Account
+                            </span>
+                            <div className="dropdown__container">
+                              <div className="container">
+                                <div className="row">
+                                  <div className="dropdown__content col-lg-2">
+                                    <ul className="menu-vertical">
+                                      <li>
+                                        <LinkContainer to="/account/orders">
+                                          <a href="#/">Orders</a>
+                                        </LinkContainer>
+                                      </li>
+                                      <li>
+                                        <LinkContainer to="/account/settings">
+                                          <a href="#/">Settings</a>
+                                        </LinkContainer>
+                                      </li>
+                                      <li className="separate" onClick={handleLogout}>
+                                        <a href="#/">Log Out</a>
+                                      </li>
+                                    </ul>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </li>
+                        </ul>
+                      </div>
+                      <div className="bar__module">
+                        <LinkContainer to="/order">
+                          <div className="btn btn--sm btn--primary type--uppercase">
+                            <div className="btn__text">
+                              Order Now
+                            </div>
+                          </div>
+                        </LinkContainer>
+                      </div>
+                    </>
+                    : <>
+                      <div className="bar__module">
+                        <ul className="menu-horizontal text-left">
+                          <li>
+                            <LinkContainer to="/cart">
+                              <a href="#/">Cart</a>
+                            </LinkContainer>
+                          </li>
+                          <li>
+                            <LinkContainer to="/login">
+                              <a href="#/">Log In</a>
+                            </LinkContainer>
+                          </li>
+                        </ul>
+                      </div>
+                      <div className="bar__module">
+                        <LinkContainer to="/signup">
+                          <div className="btn btn--sm type--uppercase">
+                            <div className="btn__text">Sign Up</div>
+                          </div>
+                        </LinkContainer>
+                        <LinkContainer to="/order">
+                          <div className="btn btn--sm btn--primary type--uppercase">
+                            <div className="btn__text">Order Now</div>
+                          </div>
+                        </LinkContainer>
+                      </div>
+                    </>
+                  }
+                </div>
               </div>
-            </footer>
-          </div >
-      }
-    </>
+            </div>
+          </nav>
+        </div>
+      </div>
+      <Routes appProps={{ isAuthenticated, userHasAuthenticated }} />
+      <footer className="footer-3 text-center-xs space--xs ">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <img alt="logo" className="logo" src="/img/logo-dark.png" />
+              <ul className="list-inline list--hover">
+                <li className="list-inline-item">
+                  <LinkContainer to="/order">
+                    <div className="hover">
+                      <span className="type--fine-print">Get Started</span>
+                    </div>
+                  </LinkContainer>
+                </li>
+                <li className="list-inline-item">
+                  <span className="type--fine-print">hello@editmule.com</span>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-6 text-md-right text-center-xs">
+              <ul className="list-inline list--hover">
+                <li className="list-inline-item">
+                  <span className="type--fine-print">&copy; 2020 Edit Mule</span>
+                </li>
+                <li className="list-inline-item">
+                  <LinkContainer to="/privacy">
+                    <div className="hover">
+                      <span className="type--fine-print">Privacy</span>
+                    </div>
+                  </LinkContainer>
+                </li>
+                <li className="list-inline-item">
+                  <LinkContainer to="/terms">
+                    <div className="hover">
+                      <span className="type--fine-print">Terms</span>
+                    </div>
+                  </LinkContainer>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div >
   );
 }
 
