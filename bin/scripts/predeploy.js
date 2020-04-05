97d5a5f3f81271c5dc371abcac8abdc2a814f52f
zanemountcastle <zane.mountcastle@gmail.com>
2020-04-05 16:33:44 -0700

    Updated deployment script for conditional inclusion of robots.txt and sitemap.xml in build based on release stage

--- bin/scripts/predeploy.js
+++ bin/scripts/predeploy.js
@@ -2,10 +2,20 @@ const execSync = require('child_process').execSync;
 
 if (process.env.NODE_ENV === 'production') {
   console.log('\x1b[31m%s\x1b[0m', '🚨  WARNING! Deploying to production: https://www.editmule.com');
-  execSync("REACT_APP_CONFIG_MODE=production yarn build", {stdio: 'inherit'})
-  execSync("mv ./bin/etc/staging.robots.txt ./build/robots.txt", {stdio: 'inherit'})
+  execSync("REACT_APP_CONFIG_MODE=production yarn build", {stdio: 'inherit'});
+
+  console.log('\x1b[36m%s\x1b[0m', '📝  Moving production.robots.txt to build root');
+  execSync("cp ./bin/etc/production.robots.txt ./build/robots.txt", {stdio: 'inherit'});
+
+  console.log('\x1b[36m%s\x1b[0m', '🔍  Moving production.sitemap.xml to build root');
+  execSync("cp ./bin/etc/production.sitemap.xml ./build/sitemap.xml", {stdio: 'inherit'});
 } else {
   console.log('\x1b[36m%s\x1b[0m', '🚀  Deploying to staging: https://staging.editmule.com');
   execSync("REACT_APP_CONFIG_MODE=staging yarn build", {stdio: 'inherit'});
-  execSync("mv ./bin/etc/prod.robots.txt ./build/robots.txt", {stdio: 'inherit'})
+
+  console.log('\x1b[36m%s\x1b[0m', '📝  Moving staging.robots.txt to build root');
+  execSync("cp ./bin/etc/staging.robots.txt ./build/robots.txt", {stdio: 'inherit'});
+
+  console.log('\x1b[36m%s\x1b[0m', '🔍  Moving staging.sitemap.xml to build root');
+  execSync("cp ./bin/etc/staging.sitemap.xml ./build/sitemap.xml", {stdio: 'inherit'});
 }
