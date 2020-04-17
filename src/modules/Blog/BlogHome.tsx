8867d73f0f50ec0893b060b7d9c075419212316f
zanemountcastle <zane.mountcastle@gmail.com>
2020-04-17 14:49:10 -0700

    Removed bullet points from <ul> elements outside of the blog

--- src/modules/Blog/BlogHome.tsx
+++ src/modules/Blog/BlogHome.tsx
@@ -8,7 +8,7 @@ import { Switch, Route } from 'react-router-dom';
 
 export default function BlogHome(props: any) {
   return (
-    <div className="main-container">
+    <div className="main-container Blog">
       <Switch>
         <Route exact path='/blog' component={Posts} />
         <Route path='/blog/pages/:num' component={Posts} />
